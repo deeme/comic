9318107dabc9a89a8ad0eac517a82a374f1e41c7
deeme <github.io@gmail.com>
2024-03-10 18:24:23 +0800

    更改提示

--- src/app/engine/presets.ts
+++ src/app/engine/presets.ts
@@ -54,15 +54,16 @@ export const presets: Record<string, Preset> = {
     family: "asian",
     color: "color",
     font: "actionman",
-    llmPrompt: "我想让你扮演一位擅长创作魔幻现实主义故事的作家。你将用一些不直白，句式结构不重复，没有陈词滥调，不寻常的词句，隐喻和象征创作出抽象、有意境想象力、有创意个性、有力度、有画面感、有音乐感，具有浪漫气息，语言深邃的故事，来表达独特的神秘和魔幻感，表达对自我和世界的探索和反思，表达对自己和社会的孤独和关注，让读者有一种有趣、惊奇和新鲜的感觉。",
+    llmPrompt: "我想让你扮演一位擅长创作魔幻现实主义题材剧本的编剧。你将用一些不直白，句式结构不重复，没有陈词滥调，不寻常的词句，隐喻和象征创作出抽象、有意境想象力、有创意个性、有力度、有画面感、有音乐感，具有浪漫气息，语言深邃的故事，来表达独特的神秘和魔幻感，表达对自我和世界的探索和反思，表达对自己和社会的孤独和关注，让观看者有一种有趣、惊奇和新鲜的感觉。",
     imagePrompt: (prompt: string) => [
-      `Visual Novel`,
+      `Chinese cinema`,
       prompt,
-      "insanely detailed and intricate",
-      "in a symbolic and meaningful style"
+      "DVD 屏幕截图",
+      "1980s fantasy cinema"
     ],
     negativePrompt: () => [
-      "Cinematic"
+      "--ar 9:16",
+      "--style raw"
     ],
   },
   chinese_manga: {
