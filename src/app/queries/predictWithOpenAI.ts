2d396d3bd3fee5421eb2caa4572d30970bf99adb
deeme <github.io@gmail.com>
2024-11-28 11:42:15 +0800

    更改提示

--- src/app/queries/predictWithOpenAI.ts
+++ src/app/queries/predictWithOpenAI.ts
@@ -7,15 +7,15 @@ import { LLMPredictionFunctionParams } from "@/types"
 // 定义备选模型数组
 const FALLBACK_MODELS = [
   process.env.LLM_OPENAI_API_MODEL || "claude-3-5-sonnet-20240620",
-  process.env.LLM_OPENAI_API_MODEL1 || "gemini-exp-1121",
-  process.env.LLM_OPENAI_API_MODEL2 || "o1-preview",
-  process.env.LLM_OPENAI_API_MODEL3 || "o1-mini",
-  process.env.LLM_OPENAI_API_MODEL4 || "gpt-4o",
-  process.env.LLM_OPENAI_API_MODEL5 || "gpt-4o-mini",
-  process.env.LLM_OPENAI_API_MODEL6 || "claude-3-haiku-20240307",
-  process.env.LLM_OPENAI_API_MODEL7 || "lmsys/claude-3-5-sonnet-20241022",
-  process.env.LLM_OPENAI_API_MODEL8 || "lmsys/claude-3-5-haiku-20241022",
-  process.env.LLM_OPENAI_API_MODEL9 || "lmsys/claude-3-5-sonnet-20240620"
+  process.env.LLM_OPENAI_API_MODEL1 || "lmsys/claude-3-5-sonnet-20241022",
+  process.env.LLM_OPENAI_API_MODEL2 || "lmsys/claude-3-5-haiku-20241022",
+  process.env.LLM_OPENAI_API_MODEL3 || "lmsys/claude-3-5-sonnet-20240620",
+  process.env.LLM_OPENAI_API_MODEL4 || "o1-preview-all",
+  process.env.LLM_OPENAI_API_MODEL5 || "o1-mini-all",
+  process.env.LLM_OPENAI_API_MODEL6 || "gpt-4o-all",
+  process.env.LLM_OPENAI_API_MODEL7 || "gpt-4-all",
+  process.env.LLM_OPENAI_API_MODEL8 || "gpt-4o-mini",
+  process.env.LLM_OPENAI_API_MODEL9 || "claude-3-haiku-20240307"
 ]
 
 // 创建单次调用函数
